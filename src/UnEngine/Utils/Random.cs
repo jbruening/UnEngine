0f1e4199decdd117a2d579ccd20643d0d429535c
Robert Eckert <robert.eckert@games-academy.de>
2014-03-27 16:52:19 +0100

    added Random class

---
+++ src/UnEngine/Utils/Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityEngine
+{
+	public static class Random
+	{
+		private static System.Random random = new System.Random();
+		public static float Range(float min, float max)
+		{
+			return (float)(min + random.NextDouble() * (max - min));
+		}
+
+		public static int Range(int min, int max)
+		{
+			return min + random.Next(max - min);
+		}
+	}
+}
