8d1e2022be87736b7040c4e21bae26c92245b064
Robert Eckert <robert.eckert@games-academy.de>
2014-03-28 19:07:06 +0100

    Added some methods to Vector2

--- src/UnEngine/Structs/Vector2.cs
+++ src/UnEngine/Structs/Vector2.cs
@@ -1,4 +1,6 @@
-﻿#if UNENG
+﻿using System;
+
+#if UNENG
 namespace UnEngine
 #else
 namespace UnityEngine
@@ -23,5 +25,61 @@ namespace UnityEngine
             this.x = x;
             this.y = y;
         }
+
+		public static Vector2 operator +(Vector2 a, Vector2 b)
+		{
+			return new Vector2(a.x + b.x, a.y + b.y);
+		}
+
+		public static Vector2 operator -(Vector2 a, Vector2 b)
+		{
+			return new Vector2(a.x - b.x, a.y - b.y);
+		}
+
+		public static Vector2 operator -(Vector2 a)
+		{
+			return new Vector2(-a.x, -a.y);
+		}
+
+		public static Vector2 operator *(float d, Vector2 a)
+		{
+			return new Vector2(a.x * d, a.y * d);
+		}
+
+		public static Vector2 operator *(Vector2 a, float d)
+		{
+			return new Vector2(a.x * d, a.y * d);
+		}
+
+		public static Vector2 operator /(Vector2 a, float d)
+		{
+			return new Vector2(a.x / d, a.y / d);
+		}
+
+		public float magnitude
+		{
+			get
+			{
+				return (float)Math.Sqrt(x * (double)x + y * (double)y);
+			}
+		}
+
+		public static Vector2 MoveTowards(Vector2 current, Vector2 target, float maxDistanceDelta)
+		{
+			var vector2 = target - current;
+			var magnitude = vector2.magnitude;
+			if (magnitude <= (double)maxDistanceDelta || magnitude == 0.0)
+				return target;
+			return current + vector2 / magnitude * maxDistanceDelta;
+		}
+
+		public static Vector2 Lerp(Vector2 from, Vector2 to, float t)
+		{
+			if (t < 0f)
+				return from;
+			if (t > 1f)
+				return to;
+			return (to - from) * t + from;
+		}
     }
 }
