cfc227df0fe7e5ce5b4fc5cc8e7b8860230e2d2e
Benjamin Schug <benjamin.schug@gmail.com>
2014-04-02 14:01:32 +0200

    GUI.changed, GUI.DragWindow

---
+++ src/UnEngine/GUI/GUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityEngine
+{
+    public static class GUI
+    {
+        public static bool changed { get { return false; } }
+
+        public static void DragWindow (Rect position) { }
+    }
+}
