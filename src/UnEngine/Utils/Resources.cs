709e228cc0ee5cb3a966a005b0d22c798ebaff1a
Benjamin Schug <benjamin.schug@gmail.com>
2014-04-03 12:45:30 +0200

    Resources.Load serialization

--- src/UnEngine/Utils/Resources.cs
+++ src/UnEngine/Utils/Resources.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace UnityEngine
 {
 	public static class Resources
 	{
-		// TODO: actually do something
-		public static Object Load(string path, Type systemTypeInstance) { return null; }
-	}
+		public static Object Load(string path, Type systemTypeInstance) 
+        {
+            Object asset = null;
+
+            if (File.Exists (path))
+            {
+                Stream stream = File.OpenRead (path);
+                XmlSerializer deserializer = new XmlSerializer (systemTypeInstance);
+                asset = (Object)deserializer.Deserialize (stream);
+                stream.Close ();
+            }
+
+            return asset; 
+        }
+
+        public static void Store<T>(string path, T asset)
+        {
+            Stream stream = File.Create (path);
+            XmlSerializer serializer = new XmlSerializer (typeof(T));
+            serializer.Serialize (stream, asset);
+            stream.Close ();
+	    }
+    }
 }
---
+++ src/UnEngineUnitTests/ResourcesTest.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnEngineUnitTests
+{
+
+    [TestClass ()]
+    public class ResourcesTest
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        [Serializable]
+        public class TestClass : UnityEngine.Object
+        {
+            public int Alpha;
+
+            [SerializeField]
+            private int beta;
+            public int Beta { get { return beta; } }
+
+            public TestClass ()
+            {
+                Alpha = 0;
+                beta = 0;
+            }
+
+            public TestClass (int a, int b)
+            {
+                Alpha = a;
+                beta = b;
+            }
+
+        }
+
+
+        [TestMethod ()]
+        public void serializePublicFieldsTest ()
+        {
+            var testData = new TestClass (23, 42);
+            Resources.Store ("test.asset", testData);
+            var deserialized = (TestClass) Resources.Load ("test.asset", typeof(TestClass));
+
+            Assert.IsNotNull (deserialized);
+            Assert.AreEqual (testData.Alpha, deserialized.Alpha);
+        }
+
+
+        [TestMethod ()]
+        public void serializePrivateFieldsTest ()
+        {
+            var testData = new TestClass (23, 42);
+            Resources.Store ("test.asset", testData);
+            var deserialized = (TestClass)Resources.Load ("test.asset", typeof (TestClass));
+
+            Assert.IsNotNull (deserialized);
+            Assert.AreEqual (testData.Beta, deserialized.Beta);
+        }
+
+
+        [TestMethod ()]
+        public void nullIfFileNotFoundTest ()
+        {
+            if (File.Exists ("invalidFile")) {
+                File.Delete ("invalidFile");
+            }
+
+            var asset = Resources.Load ("invalidFile", typeof (TestClass));
+            Assert.IsNull (asset);
+        }
+    }
+}
