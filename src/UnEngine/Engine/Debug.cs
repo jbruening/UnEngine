14ecec2cc66ddaa51f2caa169f71bbd3627e353a
Benjamin Schug <benjamin.schug@gmail.com>
2014-04-04 18:17:08 +0200

    print Debug.Log etc to console

--- src/UnEngine/Engine/Debug.cs
+++ src/UnEngine/Engine/Debug.cs
@@ -10,48 +10,56 @@ namespace UnityEngine
     {
         public static void Log(object message)
         {
-            
+            Console.WriteLine (message.ToString ());
         }
         public static void Warning(object message)
         {
-            
+            Console.WriteLine (message.ToString ());
         }
         public static void Error(object message)
         {
-            
+            Console.WriteLine (message.ToString ());
         }
         public static void Exception(object message)
-        {   
+        {
+            Console.WriteLine (message.ToString ());
         }
 
         public static void Log(object message, Object context)
         {
+            Console.WriteLine (message.ToString ());
         }
 
         public static void LogError(object message)
         {
+            Console.WriteLine (message.ToString ());
         }
 
         public static void LogError(object message, Object context)
         {
+            Console.WriteLine (message.ToString ());
         }
 
         public static void ClearDeveloperConsole(){}
 
         public static void LogException(Exception exception)
         {
+            Console.WriteLine (exception.ToString ());
         }
 
         public static void LogException(Exception exception, Object context)
         {
+            Console.WriteLine (exception.ToString ());
         }
 
 		public static void LogWarning(object message)
 		{
+            Console.WriteLine (message.ToString ());
 		}
 
 		public static void LogWarning(object message, Object context)
 		{
+            Console.WriteLine (message.ToString ());
 		}
     }
 }
